4579dfc5f3d38456ef63707e2092bc7bedc0aaee
qq634917983 <31730612+qq634917983@users.noreply.github.com>
2022-04-28 05:47:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,8 @@
-#!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+ZZZ=“package/lean/default-settings/files/zzz-default-settings”
 
-# Modify default IP
-#sed -i 's/192.168.2.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate             # 修改IP地址为192.168.0.30
+
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile        # 选择argon为默认主题
+sed -i “s/OpenWrt /faceroo compil in $（TZ=UTC-8 date ”+%Y.%m.%d“） @ OpenWrt /g” $ZZZ # 增加个性名字 faceroo
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                         # 设置密码为空
